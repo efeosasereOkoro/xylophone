fbfcc64fc995a97d7e8f1d052a47eb5ab039af1d
efeosasereOkoro <efeosasere@gmail.com>
2022-06-10 14:46:23 +0100

    streamline the code

--- lib/main.dart
+++ lib/main.dart
@@ -6,98 +6,44 @@ void main() => runApp(const XylophoneApp());
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({Key? key}) : super(key: key);
 
+//added function
   void playSound(int soundNumber) {
     final player = AudioCache();
     player.play('note$soundNumber.wav');
+  }
+
+ Container buildKey(color, soundNumber) {
+    return Container(
+      color: color,
+      width: double.infinity,
+      height: double.infinity,
+      child: TextButton(
+        onPressed: () {
+          playSound(soundNumber);
+        },
+        child: const Text(''),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Center(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Container(
-                  color: Colors.red,
-                  width: double.infinity,
-                  height: 100,
-                  child: TextButton(
-                    onPressed: () {
-                      playSound(1);
-                    },
-                    child: const Text(''),
-                  ),
-                ),
-                Container(
-                  color: Colors.orange,
-                  width: double.infinity,
-                  height: 100,
-                  child: TextButton(
-                    onPressed: () {
-                      playSound(2);
-                    },
-                    child: const Text(''),
-                  ),
-                ),
-                Container(
-                  color: Colors.yellow,
-                  width: double.infinity,
-                  height: 100,
-                  child: TextButton(
-                    onPressed: () {
-                      playSound(3);
-                    },
-                    child: const Text(''),
-                  ),
-                ),
-                Container(
-                  color: Colors.green,
-                  width: double.infinity,
-                  height: 100,
-                  child: TextButton(
-                    onPressed: () {
-                      playSound(4);
-                    },
-                    child: const Text(''),
-                  ),
-                ),
-                Container(
-                  color: Colors.blue,
-                  width: double.infinity,
-                  height: 100,
-                  child: TextButton(
-                    onPressed: () {
-                      playSound(5);
-                    },
-                    child: const Text(''),
-                  ),
-                ),
-                Container(
-                  color: Colors.indigo,
-                  width: double.infinity,
-                  height: 100,
-                  child: TextButton(
-                    onPressed: () {
-                      playSound(6);
-                    },
-                    child: const Text(''),
-                  ),
-                ),
-                Container(
-                  color: Colors.purple,
-                  width: double.infinity,
-                  height: 100,
-                  child: TextButton(
-                    onPressed: () {
-                      playSound(7);
-                    },
-                    child: const Text(''),
-                  ),
-                ),
+                buildKey(Colors.red, 1),
+                buildKey(Colors.orange, 2),
+                buildKey(Colors.yellow, 3),
+                buildKey(Colors.green, 4),
+                buildKey(Colors.teal, 5),
+                buildKey(Colors.blue, 6),
+                buildKey(Colors.purple, 7),
               ],
             ),
           ),
